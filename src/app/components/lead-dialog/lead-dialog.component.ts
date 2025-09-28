b1040908a791090649f47146488a67150fbc6b4d
ManikantaCh <manikantacheduluri89@gmail.com>
2025-09-28 15:01:50 +0530

    Updated styles and clened assets

--- src/app/components/lead-dialog/lead-dialog.component.ts
+++ src/app/components/lead-dialog/lead-dialog.component.ts
@@ -6,11 +6,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { ApiService } from '../../../services/api.service';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -30,8 +25,6 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LeadDialogComponent {
   visitForm: FormGroup;
-  isDatepickerOpen = false;
-  googleScriptUrl = 'https://script.google.com/macros/s/AKfycbwXgzIfZZ9mOQxvsL2vrVVedkyWMLQtoX-OAQJhKm4ZQHWWz3dilOAzEFMxdeh0R0hA/exec';
   
   @HostListener('document:mousedown', ['$event'])
   onClickOutside(event: MouseEvent) {
@@ -52,8 +45,6 @@ export class LeadDialogComponent {
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<LeadDialogComponent>,
-    private snackBar: MatSnackBar,
-    private api: ApiService,
     private http: HttpClient
   ) {
     this.visitForm = this.fb.group({
@@ -64,103 +55,20 @@ export class LeadDialogComponent {
     });
   }
 
-      // base = environment.apiBase;
-      // scheduleVisit(body: { name: string; email: string; date: Date; phone: string; message?: string }) {
-      //   return this.http.post(`${this.base}/visits`, body);
-      // }
+
 
   closeModal(): void {
     this.dialogRef.close();
-  }
-
-  // async submitForm() {
-  //   if (this.visitForm.valid) {
-  //     try {
-  //       await this.api.scheduleVisit(this.visitForm.value).toPromise();
-  //       this.snackBar.open('Visit scheduled successfully!', 'Close', {
-  //         duration: 5000,
-  //         horizontalPosition: 'center',
-  //         verticalPosition: 'bottom'
-  //       });
-  //       this.dialogRef.close(true);
-  //     } catch (error) {
-  //       this.snackBar.open('Failed to schedule visit. Please try again.', 'Close', {
-  //         duration: 5000
-  //       });
-  //     }
-  //   } else {
-  //     this.visitForm.markAllAsTouched();
-  //   }
-  // }
-
-  //  submitForm() {
-  //   if (this.visitForm.valid) {
-  //     this.http.post(this.googleScriptUrl, this.visitForm.value).subscribe({
-  //       next: () => {
-  //         this.snackBar.open('Visit scheduled successfully!', 'Close', {
-  //           duration: 5000,
-  //           horizontalPosition: 'center',
-  //           verticalPosition: 'bottom'
-  //         });
-  //         this.dialogRef.close(true);
-  //       },
-  //       error: () => {
-  //         this.snackBar.open('Failed to schedule visit. Please try again.', 'Close', {
-  //           duration: 5000
-  //         });
-  //       }
-  //     });
-  //   } else {
-  //     this.visitForm.markAllAsTouched();
-  //   }
-  // }
-
-//   submitForm() {
-//   if (this.visitForm.valid) {
-//     const params = new URLSearchParams(this.visitForm.value).toString();
-//     this.http.get(`${this.googleScriptUrl}?${params}`).subscribe({
-//       next: () => {
-//         this.snackBar.open('Visit scheduled successfully!', 'Close', { duration: 5000 });
-//         this.dialogRef.close(true);
-//       },
-//       error: () => {
-//         this.snackBar.open('Failed to schedule visit. Please try again.', 'Close', { duration: 5000 });
-//       }
-//     });
-//   } else {
-//     this.visitForm.markAllAsTouched();
-//   }
-// }
-
-// submitForm() {
-//   if (this.visitForm.valid) {
-//     const params = new URLSearchParams(this.visitForm.value).toString();
-//     this.http.jsonp(`${this.googleScriptUrl}?${params}`, 'callback').subscribe({
-//       next: () => {
-//         alert('Visit scheduled successfully!');
-//         this.dialogRef.close(true);
-//       },
-//       error: () => {
-//         alert('Failed to schedule visit. Please try again.');
-//       }
-//     });
-//   } else {
-//     this.visitForm.markAllAsTouched();
-//   }
-// }
-
+  } 
 submitForm() {
   if (this.visitForm.valid) {
     this.http.post('/.netlify/functions/saveLead', this.visitForm.value).subscribe({
       next: () => {
-        // this.snackBar.open('Visit scheduled successfully!', 'Close', { duration: 5000 });
         alert('Visit scheduled successfully!');
         this.dialogRef.close(true);
       },
       error: () => {
-        console.error('Failed to schedule visit. Please try again.');
         alert('Failed to schedule visit. Please try again.');
-        // this.snackBar.open('Failed to schedule visit. Please try again.', 'Close', { duration: 5000 });
       }
     });
   } else {
